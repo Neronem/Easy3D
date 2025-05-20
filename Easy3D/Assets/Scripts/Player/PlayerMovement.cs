ded9b55508ce81c31ad0343e5baac59396914d00
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-05-20 20:21:34 +0900

    ✨[feat]플레이어 이동, 회전 구현
    WASD 이동, 마우스 회전 구현

--- Easy3D/Assets/Scripts/Player/PlayerMovement.cs
+++ Easy3D/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,78 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovement;
+
+    [Header("Look")] 
+    public Transform cameraContainer;
+    public float minXLook;
+    public float maxXLook;
+    private float camCurXRot;
+    public float lookSensitivity;
+    private Vector2 mouseDelta;
+    
+    private Rigidbody _rigidbody;
+
     void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        CameraLook();
+    }
+
+    void Move()
+    {
+        Vector3 dir = transform.forward * curMovement.y + transform.right * curMovement.x;
+        dir *= moveSpeed;
+        dir.y = _rigidbody.velocity.y;
         
+        _rigidbody.velocity = dir;
+    }
+
+    void CameraLook()
+    {
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+        
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+    }
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovement = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovement = Vector2.zero;
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
     }
 }
