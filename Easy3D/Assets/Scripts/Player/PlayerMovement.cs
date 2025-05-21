ac3d79968c936dd0693368a9f25560aeb70cf176
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-05-21 11:12:13 +0900

    ✨[feat] 플레이어 움직임 구현
    InputManager 활용한 플레이어 움직임 (점프 포함), 애니메이션 적용

--- Easy3D/Assets/Scripts/Player/PlayerMovement.cs
+++ Easy3D/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,8 +8,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed;
+    public float jumpForce;
     private Vector2 curMovement;
-
+    public LayerMask groundLayer;
+    
     [Header("Look")] 
     public Transform cameraContainer;
     public float minXLook;
@@ -18,10 +20,12 @@ public class PlayerMovement : MonoBehaviour
     public float lookSensitivity;
     private Vector2 mouseDelta;
     
+    private Animator animator;
     private Rigidbody _rigidbody;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -64,15 +68,50 @@ public class PlayerMovement : MonoBehaviour
         if (context.phase == InputActionPhase.Performed)
         {
             curMovement = context.ReadValue<Vector2>();
+            
+            animator.SetBool("MoveForward", curMovement.y > 0);
+            animator.SetBool("MoveBackward", curMovement.y < 0);
+            animator.SetBool("MoveLeftOrRight", curMovement.y == 0 && curMovement.x != 0);
         }
         else if (context.phase == InputActionPhase.Canceled)
         {
             curMovement = Vector2.zero;
+            animator.SetBool("MoveForward", false);
+            animator.SetBool("MoveBackward", false);
+            animator.SetBool("MoveLeftOrRight", false);
         }
     }
 
     public void OnLook(InputAction.CallbackContext context)
     {
         mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && IsGround())
+        {
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    bool IsGround()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayer))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
